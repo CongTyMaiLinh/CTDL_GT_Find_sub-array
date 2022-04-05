3df86d2d62ceb88a154e6b1a5d020ee3479055df
CongTyMaiLinh <100610960+CongTyMaiLinh@users.noreply.github.com>
2022-04-05 11:20:40 +0700

    Update SubArr_update.cpp

--- SubArr_update.cpp
+++ SubArr_update.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void find_sub_array_max() {
-	int n;
-	cout << "Nhap kich thuoc mang = "; cin  >> n;
+int *Nhap_mang(int n) {
 	int arr[n];
 	int i;
 	for (i = 0; i < n; i++) {
 		cout << "Mhap phan tu mang thu " << i << ": "; cin >> arr[i];
 	}
+	return arr;
+}
+void find_sub_array_max(int n,int *arr) {
 	int width_max = INT_MIN;
 	int WM_start = 0, WM_last = 0, index_start = 0;
-	int sum = 0;
+	int i, sum = 0;
 	for (i = 0; i < n; i++) {
 		if(sum +arr[i] < arr[i]) {
 			index_start = i;
@@ -29,6 +30,8 @@ void find_sub_array_max() {
     cout << "start from " << WM_start << " to " << WM_last << "\n";
 }
 int main() {
-	find_sub_array_max();
+	int n;
+	cout << "Nhap kich thuoc mang = "; cin  >> n;
+	find_sub_array_max(n, Nhap_mang(n));
 	return 0;
 }
